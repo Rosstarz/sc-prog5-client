0fa4fa47b325ea0e7be031440b1b07257b592efa
rosstarz <rqr707@gmail.com>
2024-06-13 00:12:15 +0000

    testing proxy

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,11 @@ const config = {
         watchFiles: ['src/**/*'],
         // compress: true,
         port: 9000,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
         proxy: [
             {
                 context: ['/api'],
